54928ffeb6b65c57ef4e62c0c0bb61339ea94dd2
Geoff Kinder <geoff.kinder@gmail.com>
2011-10-05 10:28:52 -0500

    Initial site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kidstop::Application.config.secret_token = '0d2f3812c662ee0c2e94f6f5c6bd305d82e5b7a3edbbc1f7e99cfa263fcbaf70e04a0c46e70361fc7a1a1b4cf33cc883581a2d05c4ba789c1749d2da3796e462'
