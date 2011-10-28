6c73a1921a1dcca7f62ee67207ef9bf492b28347
Geoff Kinder <geoff.kinder@gmail.com>
2011-10-27 20:00:14 -0500

    Added flash to footer.

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,7 +1,13 @@
 class NewsletterController < ApplicationController
   def subscribe
     mimi = MadMimi.new(ENV['MADMIMI_EMAIL'], ENV['MADMIMI_KEY'])
-    mimi.add_to_list(params[:newsletter], "Newsletter")
+    result = mimi.add_to_list(params[:newsletter], "Newsletter")
+    
+    if result.empty?
+      flash[:success] = "Welcome To Our Mailing List"
+    else
+      flash[:error] = "Invalid Email"
+    end
     
     redirect_to :back
   end
